7c1a8f86173b508134b509b35b18c724d2b96041
jlethuau <julien.le.thuaut@gmail.com>
2013-03-21 16:38:03 +0100

    Add JS script to manage file upload

--- js/admin.js
+++ js/admin.js
@@ -2,5 +2,8 @@
 	"use strict";
 	$(function () {
 		// Place your administration-specific JavaScript here
+		if($('#media-meta').length > 0) {
+			$('form').attr('enctype', 'multipart/form-data');
+		} // end if
 	});
 }(jQuery));
